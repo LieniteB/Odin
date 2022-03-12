3ffb65c13883c8c68e6f2cb6de2656308e48e818
LieniteB <liene.arnicane@gmail.com>
2022-03-12 19:49:10 +0200

    Rock, paper, scissors UI

--- rock_paper_game/script.js
+++ rock_paper_game/script.js
@@ -1,4 +1,4 @@
-function computerPLay() {
+function computerPlay() {
     let num = Math.random()
     if (num < 0.33) {
         return "R"
@@ -7,56 +7,106 @@ function computerPLay() {
         return "P"
     }else return "S"
 }
-function playerPlay() {
-    let playerSel = prompt("Choose rock(R), paper(P) or scissors(S)")
-    return playerSel.toUpperCase()
-}
 
 let player = 0;
 let computer = 0;
-function singleRound(playerSelection, computerSelection) {
+const div = document.querySelector("#display");
+const h2 = document.createElement("h2");
+function playRound(playerSelection, computerSelection) {
    if (playerSelection === "R" && computerSelection === "S") {
-       alert("You won! Rock beats scissors!")
+        h2.textContent = "You won! Rock beats scissors!"
+        div.appendChild(h2)
        return player += 1
    }else if (playerSelection === "R" && computerSelection === "P") {
-       alert("You lost! Paper beats scissors!")
+        h2.textContent = "You lost! Paper beats scissors!"
+        div.appendChild(h2)
        return computer += 1
    }else if (playerSelection === "R" && computerSelection === "R") {
-        alert("Nobody wins this round! Try again!")
+        h2.textContent = "Nobody wins this round! Try again!"
+        div.appendChild(h2)
         return "Tie"
    }else if (playerSelection === "P" && computerSelection === "S") {
-        alert("You lost! Scissors beats paper!")
+        h2.textContent = "You lost! Scissors beats paper!"
+        div.appendChild(h2)
         return computer += 1
    }else if (playerSelection === "P" && computerSelection === "P") {
-        alert("Nobody wins this round! Try again")
+        h2.textContent = "Nobody wins this round! Try again!"
+        div.appendChild(h2)
         return "Tie"
    }else if (playerSelection === "P" && computerSelection === "R") {
-        alert("You won! Paper beats rock!")
+        h2.textContent = "You won! Paper beats rock!"
+        div.appendChild(h2)
         return player += 1
    }else if (playerSelection === "S" && computerSelection === "S") {
-        alert("Nobody wins this round! Try again!")
+        h2.textContent = "Nobody wins this round! Try again!"
+        div.appendChild(h2)
         return "Tie"
    }else if (playerSelection === "S" && computerSelection === "P") {
-        alert("You won! Scissors beats paper!")
+        h2.textContent = "You won! Scissors beats paper!"
+        div.appendChild(h2)
         return player += 1
    }else if (playerSelection === "S" && computerSelection === "R") {
-        alert("You lost! Rock beats scissors")
+        h2.textContent = "You lost! Rock beats scissors"
+        div.appendChild(h2)
         return computer += 1
    }else alert("Error! Please write only the letter - R, P or S.")
         return "Tie"
 }
 
-function game(){
-    for(i=0;i<5;i++){
-        if(singleRound(playerPlay(), computerPLay()) == "Tie") {
-            i--
-        }
-        console.log(player, computer)
-    }console.log(`Result is player:${player} and computer:${computer}`)
+    let rock = document.querySelector("#rock");
+    rock.addEventListener("click", () => {
+        playRound("R", computerPlay())
+        keepScore ()
+        gameOver()
+    })
 
-    if (player>computer) {
-        console.log("You won against the computer!")
-    }else console.log("You lost aginst the computer!")
+    let paper = document.querySelector("#paper");
+    paper.addEventListener("click", () => {
+        playRound("P", computerPlay())
+        keepScore ()
+        gameOver()
+        
+    })
+    
+    let scissors = document.querySelector("#scissors");
+    scissors.addEventListener("click", () => {
+        playRound("S", computerPlay())
+        keepScore ()
+        gameOver()
+    })
+
+const finalScore = document.createElement("h1");
+
+function gameOver() {
+    if (player==5) {
+        finalScore.textContent = `You won! ${computer}/${player}`;
+        div.appendChild(finalScore);
+        if (player==6 || computer==1){
+            restart()
+        }
+        
+    }else if (computer==5) {
+        finalScore.textContent = `You lost! ${computer}/${player}`;
+        div.appendChild(finalScore);
+        if (player==1 || computer==6){
+            restart()
+        }
+    }
+}
+const computerScore  = document.createElement("p");
+const playerScore  = document.createElement("p");
+
+function keepScore () {
+        computerScore.textContent = `Computer score: ${computer}`
+        div.appendChild(computerScore);
+        
+        playerScore.textContent = `Player score: ${player}`
+        div.appendChild(playerScore);
 }
 
-game();
+
+function restart() {
+    computer = 0;
+    player = 0;
+    div.removeChild(finalScore)
+}
